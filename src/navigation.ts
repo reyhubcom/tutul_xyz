8ecbaf10b268e38c09c3bbf04242e8ee3b9eb9e1
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-01-31 18:44:30 -0500

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -115,7 +115,7 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+  actions: [{ text: 'EXCHANGE', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
@@ -177,6 +177,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="#"> Rey</a> · 2026 Tutul.xyz All Rights Reserved.
   `,
 };
