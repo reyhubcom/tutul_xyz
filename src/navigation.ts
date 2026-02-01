063d23dc63b514d2aeb95ac74cc94ac106cd22b1
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-02-01 09:21:25 -0500

    1

--- src/navigation.ts
+++ src/navigation.ts
@@ -115,7 +115,7 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'EXCHANGE', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+  actions: [{ text: 'EXCHANGE', href: '/home/exchange', target: '_blank' }],
 };
 
 export const footerData = {
